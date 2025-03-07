8743c06185102a8877bee3f207d16be7904d590f
FaizMaulidin <famuld14@gmail.com>
2025-03-07 14:35:54 +0700

    test

---
+++ script.js
@@ -0,0 +1,19 @@
+// Expected result : [[1, 3, 5, 7, 9], [2, 4, 6, 8, 10]]
+// Direction : Return nested array first is odd value and second is even array from the array number
+const number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+function result(number) {
+  // Your Code Here
+  let odd = []
+  let even = []
+  number.map(num => {
+    if(num % 2 === 0){
+        even.push(num)
+    } else{
+        odd.push(num)
+    }
+  })
+  return [odd, even]
+}
+
+console.log(result(number));
